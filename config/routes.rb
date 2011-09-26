4c7628a9eee68ddf6687b06f5f41e5757ae4e988
Ryutaro Yada <ryuyada@gmail.com>
2011-09-26 17:53:52 +0900

    Add Users controller and signup page

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 SampleApp2::Application.routes.draw do
+  
+  root              :to => 'pages#home'
   match '/contact', :to => 'pages#contact'
   match '/about',   :to => 'pages#about'
   match '/help',    :to => 'pages#help'
-  root              :to => 'pages#home'
+  
+  match '/signup',  :to => 'users#new'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
